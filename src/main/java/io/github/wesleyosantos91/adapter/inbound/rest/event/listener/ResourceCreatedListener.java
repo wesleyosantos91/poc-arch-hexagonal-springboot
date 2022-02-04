b48460cba0cb4967ee332187688b1395c58547b4
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2022-02-03 23:29:04 -0300

    ajuste

--- src/main/java/io/github/wesleyosantos91/adapter/inbound/rest/event/listener/ResourceCreatedListener.java
+++ src/main/java/io/github/wesleyosantos91/adapter/inbound/rest/event/listener/ResourceCreatedListener.java
@@ -14,12 +14,12 @@ public class ResourceCreatedListener implements ApplicationListener<ResourceCrea
     @Override
     public void onApplicationEvent(ResourceCreatedEvent resourceCreatedEvent) {
         HttpServletResponse response = resourceCreatedEvent.getResponse();
-        Long codigo = resourceCreatedEvent.getId();
+        Long id = resourceCreatedEvent.getId();
 
-        adicionarHeaderLocation(response, codigo);
+        addHeaderLocation(response, id);
     }
 
-    private void adicionarHeaderLocation(HttpServletResponse response, Long id) {
+    private void addHeaderLocation(HttpServletResponse response, Long id) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
                 .buildAndExpand(id).toUri();
         response.setHeader("Location", uri.toASCIIString());
